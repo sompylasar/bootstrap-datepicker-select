31e73cc5f4ae1c024b2816afd428ba580fc402b0
John Babak <babak.john@gmail.com>
2013-03-18 00:51:54 +0400

    Fixed unintended overriding of the i18n data.

--- js/bootstrap-datepicker-select.js
+++ js/bootstrap-datepicker-select.js
@@ -19,16 +19,7 @@
 
 !function( $, undefined ) {
 	var DPGlobal = $.fn.datepicker.DPGlobal;
-	var dates = $.fn.datepicker.dates = {
-		en: {
-			days: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
-			daysShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-			daysMin: ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"],
-			months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-			monthsShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-			today: "Today"
-		}
-	};
+	var dates = $.fn.datepicker.dates;
 
 	var Dropdown_toggle = '[data-toggle=dropdown]';
 
